83bc0871acf34d661a5170be97b14f2d2a26fa31
ATengelics <Baychuks@gmail.com>
2019-11-25 18:07:10 -0500

    react source folder
    Source folder for the react version

---
+++ src/App.js
@@ -0,0 +1,26 @@
+//jshint esversion: 6
+
+import React from 'react';
+import './App.css';
+import Heading from "./components/Heading";
+import NavBar from "./components/NavBar";
+import SearchTool from "./components/SearchTool";
+import Table from "./components/Table";
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Heading />
+      <SearchTool />
+      <Table />
+      
+    </div>
+  );
+}
+
+
+
+
+export default App;
